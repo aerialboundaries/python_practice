95b79119715c1fc73642df4deb453fa771d27f1d
Masato Takatani <takatani.masato@gmail.com>
2019-11-05 16:21:53 -0600

    automate-xkcd

--- automate/lucky.py
+++ automate/lucky.py
@@ -10,10 +10,11 @@ res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
 
 # Retrieve top search result links.
-soup = bs4.BeautifulSoup(res.text)
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+# print(soup.prettify())
 
 # open a browser tab for each result.
-linkElems = soup.select('.KCrYT a')
+linkElems = soup.select(".kCrYT > a")
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
     webbrowser.open('http://google.com' + linkElems[i].get('href'))
