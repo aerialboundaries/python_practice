fa5300d69f979a8f57ce62950b0c2d9f4b56a6b5
Masato Takatani <takatani.masato@gmail.com>
2019-11-22 17:34:26 -0600

    decorator

--- test.py
+++ test.py
@@ -1,85 +1,104 @@
-# import threading
-# import time
+# def deco(func):
+#     def wrapper(*args, **kwargs):
+#         print('--start--')
+#         func(*args, **kwargs)
+#         print('---end---')
+#     return wrapper
+
+# @deco
+# def test():
+#     print('Hello, Decorator')
+
+# test()
 
 
-# global_counter = 5
+# def deco2(func):
+#     import os
+
+#     def wrapper(*args, **kwargs):
+#         res = '--start--' + os.linesep
+#         res += func(*args, **kwargs) + '!' + os.linesep
+#         res += '---end---'
+#         return res
+#     return wrapper
 
 
-# class MyThread(threading.Thread):
-#     def __init__(self, name, sleep_time):
-#         threading.Thread.__init__(self)
-#         self.name = name
-#         self.sleep_time = sleep_time
-
-#     def run(self):
-#         global global_counter
-
-#         # read
-#         count = global_counter
-#         print(self.name + ': read gloval_value ' + str(global_counter))
-
-#         # do something
-#         print(self.name + ': do something')
-#         time.sleep(self.sleep_time)
-
-#         # write
-#         global_counter = count - 1
-#         print(self.name + ': write global_value ' + str(global_counter))
+# @deco2
+# def test2():
+#     return('Hello, Decorator')
 
 
-# thread1 = MyThread('A', 5)
-# thread2 = MyThread('B', 3)
-# thread1.start()
-# time.sleep(1)
-# thread2.start()
+# print(test2())
 
-# thread1.join()
-# thread2.join()
-# print('Result: ' + str(global_counter))
-
-import threading
-import time
+# def deco_html(func):
+#     def wrapper(*args, **kwargs):
+#         res = '<html>'
+#         res = res+func(*args, **kwargs)
+#         res = res+'</html>'
+#         return res
+#     return wrapper
 
 
-global_counter = 5
-global_lock = threading.Lock()
+# def deco_body(func):
+#     def wrapper(*args, **kwargs):
+#         res = '<body>' + func(*args, **kwargs) + '</body>'
+#         return res
+#     return wrapper
 
 
-class MyThread(threading.Thread):
-    def __init__(self, name, sleep_time):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.sleep_time = sleep_time
-
-    def run(self):
-        global global_counter
-        global global_lock
-
-        # Lock
-        global_lock.acquire()
-
-        # read
-        count = global_counter
-        print(self.name + ': read gloval_value ' + str(global_counter))
-
-        # do something
-        print(self.name + ': do something')
-        time.sleep(self.sleep_time)
-
-        # write
-        global_counter = count - 1
-        print(self.name + ': write global_value ' + str(global_counter))
-
-        # Release
-        global_lock.release()
+# @deco_html
+# @deco_body
+# def test():
+#     return 'Hello, Decorator'
 
 
-thread1 = MyThread('A', 5)
-thread2 = MyThread('B', 3)
-thread1.start()
-time.sleep(1)
-thread2.start()
+# print(test())
 
-thread1.join()
-thread2.join()
-print('Result: ' + str(global_counter))
+
+# def deco_p(func):
+#     def wrapper(*args, **kwargs):
+#         res = '<p>'
+#         res = res + func(args[0], **kwargs)
+#         res = res + '</p>'
+#         return res
+#     return wrapper
+
+
+# @deco_p
+# def test(str):
+#     return(str)
+
+
+# print(test('Hello, Decorator'))
+
+
+# def deco_tag(tag):
+#     def _deco_tag(func):
+#         def wrapper(*args, **kwargs):
+#             res = '<' + tag + '>' + func(*args, **kwargs) + '</' + tag + '>'
+#             return res
+#         return wrapper
+#     return _deco_tag
+
+
+# @deco_tag('html')
+# @deco_tag('body')
+# def test():
+#     return 'Hello World'
+
+
+# print(test())
+
+
+def twice(func):
+    def wrapper(*args, **kwargs):
+        return(func(*args, *kwargs)) * 2
+    return wrapper
+
+
+@twice
+def add(x, y):
+    return x + y
+
+
+print(add(1, 3))
