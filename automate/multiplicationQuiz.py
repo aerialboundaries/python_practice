173b2949ac0c244c6f8ffb22a5135d0cb10dc898
Masato Takatani <takatani.masato@gmail.com>
2020-09-04 21:48:57 -0500

    aaa

--- automate/multiplicationQuiz.py
+++ automate/multiplicationQuiz.py
@@ -2,16 +2,35 @@
 
 # 8-Multiplication Quiz
 
-import pyinputplus as pyinputplus
-imort random, time
+import pyinputplus as pyip
+import random
+import time
 
 numberOfQuestions = 10
 correctAnswers = 0
 for questionNumber in range(numberOfQuestions):
 
-# Pick two random numbers:
-num1 = random.randint(0, 9)
-num2 = random.randint(0, 9)
+    # Pick two random numbers:
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
 
-prompt = '#%s: %s x %s = ' % (questionNumber, num1, num2)
+    prompt = '#%s: %s x %s = ' % (questionNumber, num1, num2)
 
+    try:
+        # Right answers are handled by allowRegexes.
+        # Wrong answers are handled by blockRegexes, with a custom message.
+        pyip.inputStr(prompt, allowRegexes=['^%s$' % (
+            num1 * num2)], blockRegexes=[('.*', 'Incorrect!')],
+            timeout=8, limit=3)
+
+    except pyip.TimeoutException:
+        print('Out of time!')
+    except pyip.RetryLimitException:
+        print('Out of tries!')
+    else:
+        # This block runs if no exceptions were raised in the try block.
+        print('Correct!')
+        correctAnswers += 1
+
+    time.sleep(1)  # Brief pause to let user see the result.
+print('Core: %s / %s' % (correctAnswers, numberOfQuestions))
