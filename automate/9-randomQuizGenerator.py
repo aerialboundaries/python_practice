bdbf5910d66bbed8597ebd8ab12acfcac5899a0a
Masato Takatani <takatani.masato@gmail.com>
2020-09-09 00:29:09 -0500

    aa

--- automate/9-randomQuizGenerator.py
+++ automate/9-randomQuizGenerator.py
@@ -8,22 +8,22 @@ import random
 
 # The quiz data. Keys are states and values are their capitals.
 capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
-'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
-'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
-'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
-'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
-'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
-'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
-'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
-'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
-'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
-'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
-'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
-'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
-'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
-'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
-'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia',
-'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+            'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+            'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+            'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+            'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+            'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+            'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+            'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+            'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+            'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
+            'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+            'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+            'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+            'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+            'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+            'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia',
+            'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
 # Generate 35 quiz files.
 for quizNum in range(35):
@@ -41,7 +41,7 @@ for quizNum in range(35):
 
     # Loop through all 50 states, making a question for each.
     for questionNum in range(50):
-        
+
         # Get right and wrong answers.
         correctAnswer = capitals[states[questionNum]]
         wrongAnswers = list(capitals.values())
@@ -51,14 +51,14 @@ for quizNum in range(35):
         random.shuffle(answerOptions)
 
         # Write the question and answer optins to the quiz file.
-        quizFile.write(f'{questionNum + 1}. What is the capital of {states[questionNum]}?\n')
+        quizFile.write(
+            f'{questionNum + 1}. What is the capital of {states[questionNum]}?\n')
         for i in range(4):
             quizFile.write(f"    {'ABCD'[i]}.{answerOptions[i]}\n")
         quizFile.write('\n')
 
-        
         # Write the answer key to a file.
-        answerKeyFile.write(f"{questionNum + 1}. {'ABCD'[answerOptions.index(correctAnswer)]}")
+        answerKeyFile.write(
+            f"{questionNum + 1}. {'ABCD'[answerOptions.index(correctAnswer)]}")
     quizFile.close()
     answerKeyFile.close()
-
